85c9867090617376bd3d46b9a1d744601e3c1dc4
jv-x-orc <juvenslumbertobadio@gmail.com>
2025-08-05 12:04:14 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
+  SESSION_ID: " drakonis~kvMEFAia#S6HiAw1Oic2z6xZH1KyKRpWGUNVgFPZIW8CsIrasnZE",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "50955335060", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
